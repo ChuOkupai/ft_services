5375fb7e6a1d6d101a0a75145c1a441fd18a8b16
Adrien Soursou <asoursou@student.42.fr>
2021-01-26 21:32:44 +0100

    Final

--- srcs/services/alpine-telegraf/config/health.sh
+++ srcs/services/alpine-telegraf/config/health.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Checks if all services stored in $FT_SERVICES_LIST are running
+readonly list=$(ps -o stat,args | awk 'NR > 1 && $1 != "Z" { print $2 }')
 for i in $FT_SERVICES_LIST; do
-	if ! pidof "$i" > /dev/null; then
+	if ! echo "$list" | grep -q "$i"; then
 		echo "$i is not running" >&2
 		exit 2
 	fi
--- srcs/services/influxdb/config/start.sh
+++ srcs/services/influxdb/config/start.sh
@@ -3,7 +3,7 @@
 while [ ! -d '/mnt/influxdb' ]; do
 	sleep 1
 done
+influxd -config /etc/influxdb.conf &
 readonly query="CREATE USER $INFLUX_USERNAME WITH PASSWORD '$INFLUX_PASSWORD' WITH ALL PRIVILEGES"
 env -i influx -execute "$query"
-influxd &
 telegraf
